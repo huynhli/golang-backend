41bccccc44af34d46ddb06212785d3df590a63a3
TrachScor6 <liamtamh@gmail.com>
2025-02-06 18:36:04 -0500

    showTasks with buttons
    disregard the commit named final push. i discovered you can make a full stack website in go just without the benefits of js, so i'm gonna make this functional and better than google tasks.
    
    - accessing string[7] gives ascii code i believe but i just want the char there so need to fix it
    - for loop isn't really running so figure out if thats because of the <li> tag or not

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 // global vars
 var taskListOne = []string{"clean desk", "clean bed", "clean shower", "clean dishes"}
 var taskListTwo = []string{"fold clothes", "fold shirts", "fold shorts", "fold socks"}
+var taskListThree = []string{"dry clothes", "dry shirts", "dry shorts", "dry socks"}
 var taskLists [][]string
 
 func main() {
@@ -24,21 +25,65 @@ func main() {
 }
 
 func welcome(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hi! Please visit /greeting or /showTasks to view the two other pages.")
+	var welcome_msg = `Hi! This is the home page. This project was built entirely 
+	with VSCode and Golang. Please visit /greeting or /showTasks to view the other pages.`
+	fmt.Fprint(writer, welcome_msg)
 }
 
 func helloUser(writer http.ResponseWriter, request *http.Request) {
-	var greeting = "Hello user!"
-	fmt.Fprintf(writer, greeting)
+	var greeting = `Hello user! This is a page specifically meant for greeting the user.
+	You can think of it like the screen you would see after logging in? However, it's purpose has 
+	been reduced to nearly nothing without proper frontend and api implementation, so 
+	now it's kind of just a random page :3`
+	fmt.Fprint(writer, greeting)
 }
 
 func showTasks(writer http.ResponseWriter, request *http.Request) {
-	taskLists = append(taskLists, taskListOne)
+	//tells client to interpret response as html
+	writer.Header().Set("Content-Type", "text/html")
 
-	taskListTwo = addTask(taskListTwo, "make dinner")
-	taskLists = append(taskLists, taskListTwo)
+	var task_msg = `<p>This is the tasks page. Click Button 1 to display Task List 1. Click Button 2 to display Task List 2.</p>`
+	fmt.Fprintln(writer, task_msg)
 
-	fmt.Fprint(writer, taskListOne)
+	//button press submits a form, name is used when submitted -> when called, sends value
+	fmt.Fprintln(writer, `
+		<form method="POST">
+			<button type="submit" name="action" value="Button 1">Button 1</button>
+			<button type="submit" name="action" value="Button 2">Button 2</button>
+		</form>
+	`)
+
+	//checks "did client click a button and submit a form (POST request)"
+	if request.Method == http.MethodPost {
+		buttonPressed := request.FormValue("action") //get button value
+		fmt.Fprintf(writer, "<h3>You clicked %s.\n</h3>", buttonPressed)
+		buttonNum := buttonPressed[7] //for simplicity
+
+		fmt.Fprintln(writer, buttonNum, buttonPressed)
+		fmt.Fprintf(writer, "<h1>Displaying Task List %s:\n</h1>", buttonNum)
+
+		var taskList []string
+		switch buttonNum {
+		case 1:
+			taskList = taskListOne
+		case 2:
+			taskList = taskListTwo
+		case 3:
+			taskList = taskListThree
+		}
+
+		for i := 0; i <= len(taskList)-1; i++ {
+			fmt.Fprintf(writer, "<li>%s</li>", taskList[i])
+		}
+
+	}
+
+	// taskLists = append(taskLists, taskListOne)
+
+	// taskListTwo = addTask(taskListTwo, "make dinner")
+	// taskLists = append(taskLists, taskListTwo)
+
+	// fmt.Fprint(writer, taskListOne)
 }
 
 //http is how data from is transferred between backend and frontend
